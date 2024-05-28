6af698f9f17885624dd7cd9c8fcf12e857e3327d
Igor <igorlfs@ufmg.br>
2024-05-28 16:19:59 -0300

    refactor(util): always expect 2 rules

--- src/layout/components/subgroups/util.py
+++ src/layout/components/subgroups/util.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 import pysubgroup as ps
 from pandas import DataFrame
 
+NUM_EXPECTED_RULES = 2
+
 
 @dataclass
 class Interval:
@@ -14,9 +16,11 @@ message_for_not_implemented_exception = "I still can't deal with non numeric fea
 
 
 def extract_subgroup_limits(
-    subgroup: ps.Conjunction, dataset_df: DataFrame, x_column: str, y_column: str
+    subgroup: ps.Conjunction, dataset_df: DataFrame
 ) -> tuple[Interval, Interval, str]:
-    rule1, _ = subgroup.selectors
+    assert len(subgroup.selectors) == NUM_EXPECTED_RULES
+
+    rule1, rule2 = subgroup.selectors
 
     if isinstance(rule1, ps.IntervalSelector):
         lower = (
@@ -36,27 +40,21 @@ def extract_subgroup_limits(
 
     first = Interval(lower, upper)
 
-    if len(subgroup.selectors) < 2:  # noqa: PLR2004
-        rule2_attribute = y_column if rule1.attribute_name == x_column else x_column
-        lower = dataset_df[rule2_attribute].min()
-        upper = dataset_df[rule2_attribute].max()
+    if isinstance(rule2, ps.IntervalSelector):
+        lower = (
+            dataset_df[rule2.attribute_name].min()
+            if rule2.lower_bound == float("-inf")
+            else rule2.lower_bound
+        )
+        upper = (
+            dataset_df[rule2.attribute_name].max()
+            if rule2.upper_bound == float("inf")
+            else rule2.upper_bound
+        )
+    elif isinstance(rule2, ps.EqualitySelector):
+        lower = upper = rule2.attribute_value
     else:
-        rule2 = subgroup.selectors[1]
-        if isinstance(rule2, ps.IntervalSelector):
-            lower = (
-                dataset_df[rule2.attribute_name].min()
-                if rule2.lower_bound == float("-inf")
-                else rule2.lower_bound
-            )
-            upper = (
-                dataset_df[rule2.attribute_name].max()
-                if rule2.upper_bound == float("inf")
-                else rule2.upper_bound
-            )
-        elif isinstance(rule2, ps.EqualitySelector):
-            lower = upper = rule2.attribute_value
-        else:
-            raise NotImplementedError(message_for_not_implemented_exception)
+        raise NotImplementedError(message_for_not_implemented_exception)
 
     second = Interval(lower, upper)
 
--- src/test/test_util.py
+++ src/test/test_util.py
@@ -1,3 +1,4 @@
+# ruff: noqa: ANN201
 import pytest
 from pandas import DataFrame
 from pysubgroup import Conjunction, EqualitySelector, IntervalSelector
@@ -12,39 +13,58 @@ A_UPPER_BOUND = 3
 
 
 @pytest.fixture
-def df() -> DataFrame:
+def df():
     return DataFrame()
 
 
-def test_extract_bounded_subgroups(df: DataFrame) -> None:
+@pytest.fixture
+def interval_a():
+    return IntervalSelector("a", A_LOWER_BOUND, A_UPPER_BOUND)
+
+
+def test_extract_bounded_subgroups(df: DataFrame, interval_a: IntervalSelector):
     selectors: list[IntervalSelector] = [
+        interval_a,
         IntervalSelector("b", B_LOWER_BOUND, B_UPPER_BOUND),
-        IntervalSelector("a", A_LOWER_BOUND, A_UPPER_BOUND),
     ]
 
     subgroup = Conjunction(selectors)
 
-    rule1, rule2, _ = extract_subgroup_limits(subgroup, df, "", "")
+    rule1, rule2, _ = extract_subgroup_limits(subgroup, df)
 
-    assert rule1.lower == B_LOWER_BOUND
-    assert rule1.upper == B_UPPER_BOUND
+    assert rule1.lower == A_LOWER_BOUND
+    assert rule1.upper == A_UPPER_BOUND
 
-    assert rule2.lower == A_LOWER_BOUND
-    assert rule2.upper == A_UPPER_BOUND
+    assert rule2.lower == B_LOWER_BOUND
+    assert rule2.upper == B_UPPER_BOUND
 
 
-def test_extract_equality_selection(df: DataFrame) -> None:
-    selectors: list = [
+def test_extract_equality_selection(df: DataFrame, interval_a: IntervalSelector):
+    selectors: list[IntervalSelector | EqualitySelector] = [
+        interval_a,
         EqualitySelector("b", B_LOWER_BOUND),
-        IntervalSelector("a", A_LOWER_BOUND, A_UPPER_BOUND),
     ]
 
     subgroup = Conjunction(selectors)
 
-    rule1, rule2, _ = extract_subgroup_limits(subgroup, df, "", "")
+    rule1, rule2, _ = extract_subgroup_limits(subgroup, df)
 
-    assert rule1.lower == B_LOWER_BOUND
-    assert rule1.upper == B_LOWER_BOUND
+    assert rule1.lower == A_LOWER_BOUND
+    assert rule1.upper == A_UPPER_BOUND
 
-    assert rule2.lower == A_LOWER_BOUND
-    assert rule2.upper == A_UPPER_BOUND
+    assert rule2.lower == B_LOWER_BOUND
+    assert rule2.upper == B_LOWER_BOUND
+
+
+def test_too_many_selectors(df: DataFrame, interval_a: IntervalSelector):
+    selectors: list[IntervalSelector] = [interval_a, interval_a, interval_a]
+    subgroup = Conjunction(selectors)
+    with pytest.raises(AssertionError):
+        extract_subgroup_limits(subgroup, df)
+
+
+def test_too_few_selectors(df: DataFrame):
+    selectors: list[IntervalSelector] = []
+    subgroup = Conjunction(selectors)
+    with pytest.raises(AssertionError):
+        extract_subgroup_limits(subgroup, df)
